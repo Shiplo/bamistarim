b6660532ad9ee4cf48ef75a4471578d9651c9f6a
Shiplo Rahman <shiplo.msi2015@gmail.com>
2022-10-25 22:25:42 +0600

    Deploy my react app to github pages

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,8 @@
 import React from 'react'
-import Header from '../src/components/Header'
 
 function About() {
   return (
     <div>
-        <Header />
         <h1 className="p-4 bg-white">About Page</h1>
     </div>
   )
